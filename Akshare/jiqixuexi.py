d1b08b8e8db7c2d324df4584ffbe3289d8a0f1dd
soj1998 <736495034@qq.com>
2023-03-06 08:43:48 +0800

    0303

--- Akshare/jiqixuexi.py
+++
@@ -1,6 +0,0 @@
-from __future__ import print_function, absolute_import, unicode_literals
-from datetime import datetime
-import numpy as np
-from gm.api import *
-import sys
-from sklearn import svm
